c1f3785e59e13e77cebd09728829f6d3478d6c29
unknown <gillanbrinner547@gmail.com>
2025-09-30 00:41:01 +0700

    perbaikan

---
+++ Commands/Pushup.lua
@@ -0,0 +1,17 @@
+-- Pushup.lua (ketika dieksekusi kirim chat "Siap laksanakan!")
+return {
+    Execute = function(msg, client)
+        local vars = _G.BotVars or {}
+        local TextChatService = vars.TextChatService or game:GetService("TextChatService")
+
+        -- Kirim chat ke RBXGeneral
+        local channel = TextChatService.TextChannels and TextChatService.TextChannels:FindFirstChild("RBXGeneral")
+        if channel then
+            pcall(function()
+                channel:SendAsync("Siap laksanakan!")
+            end)
+        else
+            warn("Channel RBXGeneral tidak ditemukan!")
+        end
+    end
+}
