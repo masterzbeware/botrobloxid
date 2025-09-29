fc6ea0dae29aa96178c38ac95b50c51cf3d429d7
unknown <gillanbrinner547@gmail.com>
2025-09-29 19:03:06 +0700

    perbaikan

--- Commands/Punishment.lua
+++ Commands/Punishment.lua
@@ -1,25 +1,36 @@
-local TextChatService = game:GetService("TextChatService")
-local Players = game:GetService("Players")
+-- Punishment.lua
+return {
+    Execute = function(msg, client)
+        print("[DEBUG] Punishment command triggered by: " .. client.Name)
+        print("[DEBUG] Original msg: " .. tostring(msg))
 
-local function onMessage(msg, sender)
-    if msg:lower() == "!pushup" then
-        local channel = TextChatService.TextChannels:WaitForChild("RBXGeneral", 10)
-        if channel then
-            pcall(function()
+        -- Gunakan TextChatService dari global bot vars
+        local TextChatService = _G.BotVars.TextChatService
+        if not TextChatService then
+            warn("[DEBUG] TextChatService belum tersedia!")
+            return
+        end
+
+        -- Tunggu channel RBXGeneral muncul
+        local channel = TextChatService.TextChannels and TextChatService.TextChannels:FindFirstChild("RBXGeneral")
+        if not channel then
+            warn("[DEBUG] Channel RBXGeneral tidak ditemukan!")
+            return
+        end
+
+        -- Cek command
+        if msg:lower() == "!pushup" then
+            print("[DEBUG] !pushup command dikenali, mengirim pesan...")
+            local success, err = pcall(function()
                 channel:SendAsync("Siap laksanakan!")
             end)
+            if success then
+                print("[DEBUG] Pesan berhasil dikirim!")
+            else
+                warn("[DEBUG] Gagal mengirim pesan: " .. tostring(err))
+            end
+        else
+            print("[DEBUG] Command tidak cocok: " .. tostring(msg))
         end
     end
-end
-
--- Listener sementara untuk test
-Players.PlayerAdded:Connect(function(plr)
-    plr.Chatted:Connect(function(msg)
-        onMessage(msg, plr)
-    end)
-end)
-
--- Test untuk player lokal
-Players.LocalPlayer.Chatted:Connect(function(msg)
-    onMessage(msg, Players.LocalPlayer)
-end)
+}
