5146a56ffe062d48f2136f5b9d67702eef3d4e14
unknown <gillanbrinner547@gmail.com>
2025-09-30 14:46:10 +0700

    perbaikan

--- Bot.lua
+++ Bot.lua
@@ -51,7 +51,7 @@ debugPrint("Detected identity: " .. _G.BotVars.BotIdentity)
 
 -- Commands Loader
 local Commands = {}
-local commandFiles = { "Ikuti.lua", "Stop.lua", "Shield.lua", "Row.lua", "Sync.lua", "Pushup.lua", "Frontline.lua", "AmbilAlih.lua" }
+local commandFiles = { "Ikuti.lua", "Stop.lua", "Shield.lua", "Row.lua", "Sync.lua", "Pushup.lua", "Frontline.lua", "AmbilAlih.lua", "Reset.lua" }
 
 for _, fileName in ipairs(commandFiles) do
     local url = repoBase .. fileName
