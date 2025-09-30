f976377c6a18d2f49591dc68c6487000630050c2
unknown <gillanbrinner547@gmail.com>
2025-09-30 23:13:41 +0700

    Perbaikan

--- Bot.lua
+++ Bot.lua
@@ -8,7 +8,7 @@ local Options = Library.Options
 
 local Window = Library:CreateWindow({
     Title = "Made by MasterZ",
-    Footer = "v2.1.0",
+    Footer = "v1.0.0",
     Icon = 0,
     NotifySide = "Right",
     ShowCustomCursor = true,
