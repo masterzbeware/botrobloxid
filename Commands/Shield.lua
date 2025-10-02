e896103676d0ebdc8071671df9ecd5bccb3b1f9e
unknown <gillanbrinner547@gmail.com>
2025-10-02 08:40:57 +0700

    perbaikan

--- Commands/Shield.lua
+++ Commands/Shield.lua
@@ -76,7 +76,7 @@ return {
 
         -- Timestamp terakhir chat
         local lastWarningTime = 0
-        local warningDelay = 13 -- detik
+        local warningDelay = 17 -- detik
 
         local function moveToPosition(targetPos, lookAtPos)
             if not humanoid or not myRootPart then return end
