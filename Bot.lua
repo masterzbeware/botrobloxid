5da567effe0539d59dc35f3396592a8602dd4567
unknown <gillanbrinner547@gmail.com>
2025-10-21 13:13:51 +0700

    perbaikan

--- Bot.lua
+++ Bot.lua
@@ -36,7 +36,7 @@ _G.BotVars.MainWindow = MainWindow
 
 -- 📦 Daftar module yang akan dimuat
 local VIPCommands = {}
-local commandFiles = { "ESP.lua", "AIM.lua", "Hide.lua", "Bullet.lua","BurstDamage.lua" }
+local commandFiles = { "HeadshotAuto.lua","ESP.lua", "AIM.lua", "Hide.lua", "Bullet.lua","BurstDamage.lua" }
 
 -- 🔹 Fungsi untuk load semua module
 local function loadScripts(files, repo, targetTable)
