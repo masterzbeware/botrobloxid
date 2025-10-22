70f9afd89ecfacc903bb9b11cc601a7cf14038d6
unknown <gillanbrinner547@gmail.com>
2025-10-22 15:47:03 +0700

    perbaikan

--- Commands/Reload.lua
+++ Commands/Reload.lua
@@ -13,10 +13,10 @@ return {
         local Group = CombatTab:AddLeftGroupbox("Auto Reload")
 
         -- Variabel toggle & delay
-        vars.AutoReload = vars.AutoReload ~= false
+        vars.AutoReload = vars.AutoReload or false -- default OFF
         vars.ReloadDelay = vars.ReloadDelay or 0.2
         vars.Reloading = false
-        vars.MagCheckInterval = 0.1
+        vars.CheckInterval = 0.1
 
         local ReplicatedStorage = game:GetService("ReplicatedStorage")
         local Players = game:GetService("Players")
@@ -50,23 +50,16 @@ return {
             if vars.Reloading then return end
             vars.Reloading = true
 
-            -- Kirim event Reload dari RemoteSpy
-            RemoteEvent:FireServer("ActionActor", "b6ca2d2d-dc75-4987-b8b8-085a9a89539c", 0, "Reload", false)
+            RemoteEvent:FireServer("ActionActor", "9c2920ee-d73e-4cb5-a424-cee16330ff40", 0, "Reload", true)
 
             task.delay(vars.ReloadDelay, function()
-                RemoteEvent:FireServer("ActionActor", "cd6c81a7-3f9a-4288-baaa-eb9514dce761", 0, "Reloaded", {
-                    Capacity = 30,
-                    Name = "M4A1_Stanag_Default",
-                    Caliber = "intermediaterifle_556x45mmNATO_M855",
-                    UID = "07a4535b-fc24-48c0-9dc4-94d68dddd0df"
-                })
                 vars.Reloading = false
             end)
         end
 
         -- Loop pengecekan magazine
         task.spawn(function()
-            while task.wait(vars.MagCheckInterval) do
+            while task.wait(vars.CheckInterval) do
                 if vars.AutoReload and not vars.Reloading then
                     local char = player.Character
                     if char then
