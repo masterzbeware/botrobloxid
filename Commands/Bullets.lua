e57b8b1ab57411e8b98b925d7659b4722d412ec3
unknown <gillanbrinner547@gmail.com>
2025-10-22 22:44:10 +0700

    perbaikan

--- Commands/Bullets.lua
+++ Commands/Bullets.lua
@@ -35,7 +35,7 @@ return {
           Text = "Bullets Count",
           Default = vars.BulletsValue,
           Min = 1,
-          Max = 50,
+          Max = 30,
           Rounding = 0,
           Callback = function(v)
               vars.BulletsValue = v
