4a92e39386fd335d0deb7eea4a3941d2be1b1da2
unknown <gillanbrinner547@gmail.com>
2025-10-22 23:49:47 +0700

    perbaikan

--- Commands/WindowTab.lua
+++ Commands/WindowTab.lua
@@ -5,15 +5,22 @@ local MainWindow = _G.BotVars.MainWindow
 
 local Tabs = {}
 
+-- Info Tab
 Tabs.Info = MainWindow:AddTab("Info", "info")
 local InfoGroup = Tabs.Info:AddLeftGroupbox("Bot Info")
 InfoGroup:AddLabel("MasterZ HUB v1.0.0")
 InfoGroup:AddLabel("Script loaded")
 
+-- Combat Tab
 Tabs.Combat = MainWindow:AddTab("Combat", "crosshair")
 
+-- Utility Tab
+Tabs.Utility = MainWindow:AddTab("Utility", "tool") -- gunakan icon yang sesuai jika ada
+
+-- Visual Tab
 Tabs.Visual = MainWindow:AddTab("Visual", "eye")
 
+-- Save Tabs di global
 _G.BotVars.Tabs = Tabs
 
 print("[MasterZ HUB] WindowTab.lua loaded - Tabs siap digunakan.")
