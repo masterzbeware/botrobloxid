c053ba612dc022d229192b79a011d8c61cae4f79
masterzbeware <emailmu@contoh.com>
2025-12-06 13:43:00 +0700

    perbaikan

--- Commands/WindowTab.lua
+++ Commands/WindowTab.lua
@@ -69,7 +69,7 @@ spawn(function()
 end)
 
 -- Combat Tab
-Tabs.Combat = MainWindow:AddTab("Oven", "crosshair")
+Tabs.Main = MainWindow:AddTab("Oven", "crosshair")
 
 -- Visual Tab
 Tabs.Visual = MainWindow:AddTab("Visual", "eye")
