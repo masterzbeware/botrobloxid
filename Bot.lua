d83cfce2ecea07dd4ef42868b8b25c80c09cd4b5
masterzbeware <emailmu@contoh.com>
2026-02-22 02:55:58 +0700

    perbaikan

--- Bot.lua
+++ Bot.lua
@@ -1,17 +1,20 @@
+-- Load Venyx
 local Venyx = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/Stefanuk12/Venyx-UI-Library/main/source.lua"
 ))()
 
 local venyx = Venyx.new("MasterZ UX", 5013109572)
-local page = venyx:addPage("Autos", 5012544693)
 
--- Wajib, tapi tidak kelihatan
+-- Buat page
+local page = venyx:addPage("Auto", 5012544693)
+
+-- Section TANPA JUDUL (tampilannya bersih)
 local sec = page:addSection("")
 
+-- Toggle asli Venyx
 local aktif = false
 
-local btn = sec:addButton("Auto : OFF", function()
-    aktif = not aktif
-    btn:update("Auto : " .. (aktif and "ON" or "OFF"))
-    print("Status Auto =", aktif)
+sec:addToggle("Auto Start", false, function(v)
+    aktif = v
+    print("Auto =", v and "ON" or "OFF")
 end)
