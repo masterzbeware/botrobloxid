7320d6161fb432b1560807c7690bc7812168202f
masterzbeware <emailmu@contoh.com>
2026-02-22 02:52:09 +0700

    perbaikan

--- Bot.lua
+++ Bot.lua
@@ -1,19 +1,17 @@
--- Load Venyx
 local Venyx = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/Stefanuk12/Venyx-UI-Library/main/source.lua"
 ))()
 
--- Window
 local venyx = Venyx.new("MasterZ UX", 5013109572)
-
--- Page
 local page = venyx:addPage("Auto", 5012544693)
 
--- Section kosong (wajib supaya element bisa tampil)
+-- Wajib, tapi tidak kelihatan
 local sec = page:addSection("")
 
--- Button (ini pasti muncul)
-sec:addButton("CLICK ME", function()
-    print("Button ditekan!")
-    -- taruh function kamu di sini
+local aktif = false
+
+local btn = sec:addButton("Auto : OFF", function()
+    aktif = not aktif
+    btn:update("Auto : " .. (aktif and "ON" or "OFF"))
+    print("Status Auto =", aktif)
 end)
